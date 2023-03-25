8ad2ec926447f2f1944bd9460a95e6f5a606295d
andrewtcrooks <andrewtcrooks@gmail.com>
2023-03-25 16:38:07 -0700

    Code to create cert.p12
    creates .ssl folder in config and combines the cert.pem and key.pem into
    a single cert.p12 file in the .ssl folder.

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 set -e
 
 SSH_DIR=~/.ssh
+SSL_DIR=~/.ssl
 
 ROUTER_USER="$(bashio::config 'sslFromAsusRouter.routerUser')"
 ROUTER_IP="$(bashio::config 'sslFromAsusRouter.routerIp')"
@@ -15,6 +16,9 @@ ROUTER_RSA_KEY=$(ssh-keyscan -p ${ROUTER_PORT} -t rsa ${ROUTER_IP})
 
 echo "Creating ${SSH_DIR}"
 mkdir -p ${SSH_DIR}
+
+echo "Creating ${SSL_DIR}"
+mkdir -p ${SSL_DIR}
 
 echo "Setting id_rsa file..."
 cp /config/"${RSA_PRIVATE_KEY_PATH}" ${SSH_DIR}/id_rsa
@@ -38,6 +42,9 @@ fi
 chmod 644 ${SSH_DIR}/known_hosts;
 cat ${SSH_DIR}/known_hosts
 
-echo "sshing..."
+echo "piping ssh certificates to file ..."
 ssh ${ROUTER_USER}@${ROUTER_IP} -p ${ROUTER_PORT} "cat ${KEY_PATH_ON_ROUTER}" > /ssl/key.pem
 ssh ${ROUTER_USER}@${ROUTER_IP} -p ${ROUTER_PORT} "cat ${CERT_PATH_ON_ROUTER}" > /ssl/cert.pem
+
+echo "creating cert.p12 to send to android phone ..."
+openssl pkcs12 -export -keypbe NONE -certpbe NONE -nomaciter -passout pass: -in /ssl/cert.pem -inkey /ssl/key.pem -out ${SSL_DIR}/cert.p12
